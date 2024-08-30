3e5e9c59d99a1f04c4a3fb3dbe729a94c45bd174
SmallConfusion <smallconfusion@protonmail.com>
2024-08-30 15:56:03 -0500

    git push basic snake set up

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import "log"
+
 func main() {
-	println("Hello world!")
+	RunServer()
+}
+
+func move(state GameState) BattlesnakeMoveResponse {
+	return BattlesnakeMoveResponse{Move: "left"}
+}
+
+func start(state GameState) {
+	log.Println("Start game: ", state)
+}
+
+func end(state GameState) {
+	log.Println("End game: ", state)
 }
---
+++ server.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+const (
+	port     = "8000"
+	serverId = "smallconfusion/github/snake"
+)
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	response := BattlesnakeInfoResponse{
+		APIVersion: "1",
+		Author:     "SmallConfusion",
+		Color:      "#ff92f7",
+		Head:       "Default",
+		Tail:       "Default",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(response)
+
+	if err != nil {
+		log.Println("Error with json encoder, message: ", err)
+	}
+}
+
+func handleStart(w http.ResponseWriter, r *http.Request) {
+	state := GameState{}
+	err := json.NewDecoder(r.Body).Decode(&state)
+
+	if err != nil {
+		log.Println("Error with json decoder, message: ", err)
+	}
+
+	start(state)
+}
+
+func HandleEnd(w http.ResponseWriter, r *http.Request) {
+	state := GameState{}
+	err := json.NewDecoder(r.Body).Decode(&state)
+
+	if err != nil {
+		log.Println("Error with json decoder, message: ", err)
+	}
+
+	end(state)
+}
+
+func HandleMove(w http.ResponseWriter, r *http.Request) {
+	state := GameState{}
+	err := json.NewDecoder(r.Body).Decode(&state)
+
+	if err != nil {
+		log.Println("Error with json decoder, message: ", err)
+	}
+
+	response := move(state)
+
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(response)
+
+	if err != nil {
+		log.Println("Error with json encoder, message: ", err)
+	}
+}
+
+func withServerId(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Server", serverId)
+		next(w, r)
+	}
+}
+
+func RunServer() {
+	http.HandleFunc("/", withServerId(handleIndex))
+	http.HandleFunc("/start", withServerId(handleStart))
+	http.HandleFunc("/move", withServerId(HandleMove))
+	http.HandleFunc("/end", withServerId(HandleEnd))
+
+	log.Println("Starting server")
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
