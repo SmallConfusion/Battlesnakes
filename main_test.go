3b4672901a921dc8ac20f68cde6a1cd9f79f1c22
SmallConfusion <smallconfusion@protonmail.com>
2024-09-11 20:03:19 -0500

    Made test server only run when -server flag

--- main_test.go
+++ main_test.go
@@ -1,12 +1,19 @@
 package hatsunesnaku
 
 import (
+	"flag"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 )
 
+var runServer = flag.Bool("server", false, "Runs the server in addition.")
+
 func TestMain(t *testing.T) {
+	if !*runServer {
+		t.SkipNow()
+	}
+
 	s := gin.Default()
 	RunServer(s)
 	s.Run(":8000")
